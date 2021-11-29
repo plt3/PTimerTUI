ea35c38d2705a9a30cbc44b8cadc1c4defa20bf9
Paul Taylor <pltorigami@gmail.com>
2021-11-29 13:00:33 -0500

    Make Game::setAverages handle when there aren't enough solves to calculate averages

--- Game.cpp
+++ Game.cpp
@@ -53,38 +53,63 @@ void Game::mainloop() {
 }
 
 void Game::setAverages() {
-    // TODO: make this check for empty/not big enough lastNSolves
-    unsigned lastDeqInd = lastNSolves.size() - 1;
-    std::vector<double> shortAvgVector, longAvgVector;
-    double curTime;
-
-    for (unsigned i = 0; i < LONG_AVG_NUM; i++) {
-        curTime = lastNSolves.at(lastDeqInd - i).getTime();
-        if (i < SHORT_AVG_NUM) {
-            shortAvgVector.push_back(curTime);
+    // set shortAvg and longAvg data members to the averages they represent, or
+    // to zero if there are not enough times to calculate them
+    if (lastNSolves.size() < SHORT_AVG_NUM) {
+        shortAvg = 0;
+        longAvg = 0;
+    } else {
+        bool skipLong = false;
+        unsigned int iStop = LONG_AVG_NUM;
+        if (lastNSolves.size() < LONG_AVG_NUM) {
+            skipLong = true;
+            iStop = SHORT_AVG_NUM;
         }
-        longAvgVector.push_back(curTime);
-    }
+        unsigned lastDeqInd = lastNSolves.size() - 1;
+        std::vector<double> shortAvgVector, longAvgVector;
+        double curTime;
 
-    double shortRemove =
-        *std::min_element(shortAvgVector.begin(), shortAvgVector.end()) +
-        *std::max_element(shortAvgVector.begin(), shortAvgVector.end());
-    double longRemove =
-        *std::min_element(longAvgVector.begin(), longAvgVector.end()) +
-        *std::max_element(longAvgVector.begin(), longAvgVector.end());
-
-    double shortSum = 0, longSum = 0;
-    for (unsigned i = 0; i < longAvgVector.size(); i++) {
-        curTime = longAvgVector.at(i);
-        if (i < shortAvgVector.size()) {
-            shortSum += curTime;
+        for (unsigned i = 0; i < iStop; i++) {
+            curTime = lastNSolves.at(lastDeqInd - i).getTime();
+            if (i < SHORT_AVG_NUM) {
+                shortAvgVector.push_back(curTime);
+            }
+            if (!skipLong) {
+                longAvgVector.push_back(curTime);
+            }
         }
-        longSum += curTime;
+
+        double shortRemove =
+            *std::min_element(shortAvgVector.begin(), shortAvgVector.end()) +
+            *std::max_element(shortAvgVector.begin(), shortAvgVector.end());
+        double longRemove;
+        if (!skipLong) {
+            longRemove =
+                *std::min_element(longAvgVector.begin(), longAvgVector.end()) +
+                *std::max_element(longAvgVector.begin(), longAvgVector.end());
+        }
+
+        double shortSum = 0, longSum = 0;
+        std::vector<double> &sumVector = longAvgVector;
+        if (skipLong) {
+            sumVector = shortAvgVector;
+        }
+        for (unsigned i = 0; i < sumVector.size(); i++) {
+            curTime = sumVector.at(i);
+            if (i < shortAvgVector.size()) {
+                shortSum += curTime;
+            }
+            longSum += curTime;
+        }
+
+        shortSum -= shortRemove;
+        longSum -= longRemove;
+
+        shortAvg = shortSum / (SHORT_AVG_NUM - 2);
+        if (skipLong) {
+            longAvg = 0;
+        } else {
+            longAvg = longSum / (LONG_AVG_NUM - 2);
+        }
     }
-
-    shortSum -= shortRemove;
-    longSum -= longRemove;
-
-    shortAvg = shortSum / (SHORT_AVG_NUM - 2);
-    longAvg = longSum / (LONG_AVG_NUM - 2);
 }
--- TimerBox.cpp
+++ TimerBox.cpp
@@ -34,16 +34,26 @@ void TimerBox::updateSolveDisplay(Solve newSolve, double shortAvg,
                                   double longAvg, unsigned numShort,
                                   unsigned numLong) {
     std::string niceSolveTime = newSolve.toString();
-    std::string niceShortAvg = Solve::timeToString(shortAvg);
-    std::string niceLongAvg = Solve::timeToString(longAvg);
+    std::string niceShortAvg;
+    std::string niceLongAvg;
+
+    if (shortAvg == 0) {
+        niceShortAvg = "-";
+    } else {
+        niceShortAvg = Solve::timeToString(shortAvg);
+    }
+    if (longAvg == 0) {
+        niceLongAvg = "-";
+    } else {
+        niceLongAvg = Solve::timeToString(longAvg);
+    }
 
     std::string shortAvgString =
         "avg" + std::to_string(numShort) + ": " + niceShortAvg;
     std::string longAvgString =
         "avg" + std::to_string(numLong) + ": " + niceLongAvg;
 
-    wmove(boxPtr, 0, 0);
-    wclrtoeol(boxPtr);
+    wclear(boxPtr);
     mvwprintw(boxPtr, 0, (boxWidth - niceSolveTime.length()) / 2,
               niceSolveTime.c_str());
     mvwprintw(boxPtr, 2, (boxWidth - shortAvgString.length()) / 2,
